5ee159f13fbd0ce34705d04375c06d8e7173d5b6
Sahil Verma <sahil.verma@1mg.com>
2020-01-22 17:44:28 +0530

    Micropost Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,4 +21,12 @@ users = User.order(created_at: :desc).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 end
